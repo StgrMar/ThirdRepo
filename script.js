761492c166cdbcac5d14f77a697d0dbae94b6605
Saetgaraev Marat <stgrmar@gmail.com>
2022-08-04 22:07:09 +0300

    hard hw

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
 'use strict';
 
-let game;
+let num;
 let notify;
 
-const checkAns = function (num) {
-    return !isNaN(isFinite(num) && parseFloat(num));
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 };
 
-const asking = function (a) {
 
-    const msg = function (b) {
-        b = +prompt('Угадай число от 1 до 100');
+const try10 = function (i) {
 
-        console.log('Загаданное число ' + a);
-        console.log('Занесенное число ' + b);
+    const asking = function (a) {
 
-        if (b === 0) {
-            notify = alert('Игра окончена');
-            console.log(b);
 
-        } else if (!isFinite(b)) {
-            notify = alert('Введите число!');
-            console.log(b);
+
+        const msg = function (b) {
+            b = +prompt('Угадай число от 1 до 100');
+
+            i--;
+
+            console.log('Загаданное число ' + a);
+            console.log('Занесенное число ' + b);
+
+            if (b === 0 || i === 0) {
+                notify = confirm('Игра окончена, хотите сыграть еще?');
+                if (notify == true) {
+                    try10(10);
+                }
+
+            } else if (!isFinite(b)) {
+                notify = alert('Введите число!');
+                msg();
+
+            } else if (a !== b && b > a) {
+                notify = alert('Загаданное число меньше, осталось попыток: ' + i);
+                msg();
+
+            } else if (a !== b && b < a) {
+                notify = alert('Загаданное число больше, осталось попыток: ' + i);
+                msg();
+            } else {
+                notify = alert('Поздравляю! Вы угадали!!!');
+                notify = confirm('Игра окончена, хотите сыграть еще?');
+                if (notify == true) {
+                    try10(10);
+                }
+            }
+
+        };
+
+        if (i >= 0) {
             msg();
-
-        } else if (a !== b && b > a) {
-            notify = alert('Загаданное число меньше');
-            console.log(b);
-            msg();
-
-        } else if (a !== b && b < a) {
-            notify = alert('Загаданное число больше');
-            console.log(b);
-            msg();
-        } else {
-            notify = alert('Поздравляю! Вы угадали!!!');
-        }
-
+        };
     };
-    msg();
+    asking(random(1, 100));
 };
-asking(5);
+
+try10(10);
+
 
